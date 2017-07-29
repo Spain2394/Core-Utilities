61829d57192dd5ce8fe93b0cb64fd5735c19e755
Spaincode <aspain2394@gmail.com>
2017-07-29 02:28:06 -0400

    Add files via upload

---
+++ false.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Allen Spain on 7/29/17.
+//
+
+#include <cstdlib>
+
+int main ()
+{
+    return EXIT_FAILURE;
+}
---
+++ true.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Allen Spain on 7/29/17.
+//
+
+
+#include <cstdlib>
+
+int main ()
+{
+return EXIT_SUCCESS;
+}
