7a71c4727dbffcb3e503ab7d3bc22ec12ace91c6
Nathan Su <nathansu.w@gmail.com>
2017-07-31 22:12:25 -0400

    Add files via upload
    why does debugging this file never end?
    added the ability to continue to print the next directory if the one before it is borked

--- ls.cpp
+++ ls.cpp
@@ -46,163 +46,166 @@ struct myFiles{
  */
 
 void printDirectory( char* myDirectory, string option){
+     bool keepGoing=true;
      if((dir=opendir(myDirectory))==NULL){
           cout<<"ls: cannot access "<<myDirectory<<": "<<strerror(errno)<<endl;
           closedir(dir);
-          exit(0);
+          keepGoing=false;
      }
      closedir(dir);
-     //creating an array of strings representing the file names in a folder sorted alphabetically
-          struct dirent **namelist;
-          int n;
-          string sortedNames="";
-          string *sortedd;
-          n = scandir(myDirectory, &namelist, NULL, alphasort);
-          int totalEntries=n;
-          sortedd = new string [n];
-          if (n < 0)
-               perror("scandir");
-          else {
-               while (n--) {
-                    //cout<<n<<endl;
-                    //cout<<namelist[n]->d_name<<endl;
-                    string temp(namelist[n]->d_name);
-                    string index=to_string(n);
-                    sortedd[n]=namelist[n]->d_name;
-                    //cout<<sortedd[n]<<endl;
-                    free(namelist[n]);
+     if(keepGoing){
+          //creating an array of strings representing the file names in a folder sorted alphabetically
+               struct dirent **namelist;
+               int n;
+               string sortedNames="";
+               string *sortedd;
+               n = scandir(myDirectory, &namelist, NULL, alphasort);
+               int totalEntries=n;
+               sortedd = new string [n];
+               if (n < 0)
+                    perror("scandir");
+               else {
+                    while (n--) {
+                         //cout<<n<<endl;
+                         //cout<<namelist[n]->d_name<<endl;
+                         string temp(namelist[n]->d_name);
+                         string index=to_string(n);
+                         sortedd[n]=namelist[n]->d_name;
+                         //cout<<sortedd[n]<<endl;
+                         free(namelist[n]);
+                    }
+                    free(namelist);
                }
-               free(namelist);
-          }
-     //some really dumb stuff I did to get it sorted
+          //some really dumb stuff I did to get it sorted
 
 
 
-          int numFiles=totalEntries;
-          int counter=0;
-          if((dir=opendir(myDirectory))==NULL){
-               cout<<"Something's wrong"<<endl;
-               //return EXIT_FAILURE;
-          }
-
-          //creating and filling an array of myFiles struct objects with each respective file's information
-          myFiles * FILES;
-          FILES=new myFiles[numFiles];
-          while((dname=readdir(dir))){
-               struct passwd *pw=getpwuid(statbuff.st_uid);
-               struct group *gr=getgrgid(statbuff.st_gid);
-               stat(dname->d_name,&statbuff);
-               //cout<<dname->d_name<<endl;
-               FILES[counter].name=dname->d_name;
-               FILES[counter].permissions="";
-               FILES[counter].permissions+=((S_ISDIR(statbuff.st_mode))?"d":"-");
-               FILES[counter].permissions+=((statbuff.st_mode&S_IRUSR)?"r":"-");
-               FILES[counter].permissions+=((statbuff.st_mode&S_IWUSR)?"w":"-");
-               FILES[counter].permissions+=((statbuff.st_mode&S_IXUSR)?"x":"-");
-               FILES[counter].permissions+=((statbuff.st_mode&S_IRGRP)?"r":"-");
-               FILES[counter].permissions+=((statbuff.st_mode&S_IWGRP)?"w":"-");
-               FILES[counter].permissions+=((statbuff.st_mode&S_IXGRP)?"x":"-");
-               FILES[counter].permissions+=((statbuff.st_mode&S_IROTH)?"r":"-");
-               FILES[counter].permissions+=((statbuff.st_mode&S_IWOTH)?"w":"-");
-               FILES[counter].permissions+=((statbuff.st_mode&S_IXOTH)?"x":"-");
-               FILES[counter].numLinks=statbuff.st_nlink;
-               FILES[counter].ownerID=pw->pw_name;
-               FILES[counter].groupID=gr->gr_name;
-               FILES[counter].fileSize=statbuff.st_size;
-               FILES[counter].lastMod=statbuff.st_mtime;
-               if(dname->d_name[0]=='.'){
-                    FILES[counter].isHidden=true;
+               int numFiles=totalEntries;
+               int counter=0;
+               if((dir=opendir(myDirectory))==NULL){
+                    cout<<"Something's wrong"<<endl;
+                    //return EXIT_FAILURE;
                }
-               else{
-                    FILES[counter].isHidden=false;
-               }
-               counter++;
-          }//while loop
-          //closedir(dir);
+
+               //creating and filling an array of myFiles struct objects with each respective file's information
+               myFiles * FILES;
+               FILES=new myFiles[numFiles];
+               while((dname=readdir(dir))){
+                    struct passwd *pw=getpwuid(statbuff.st_uid);
+                    struct group *gr=getgrgid(statbuff.st_gid);
+                    stat(dname->d_name,&statbuff);
+                    //cout<<dname->d_name<<endl;
+                    FILES[counter].name=dname->d_name;
+                    FILES[counter].permissions="";
+                    FILES[counter].permissions+=((S_ISDIR(statbuff.st_mode))?"d":"-");
+                    FILES[counter].permissions+=((statbuff.st_mode&S_IRUSR)?"r":"-");
+                    FILES[counter].permissions+=((statbuff.st_mode&S_IWUSR)?"w":"-");
+                    FILES[counter].permissions+=((statbuff.st_mode&S_IXUSR)?"x":"-");
+                    FILES[counter].permissions+=((statbuff.st_mode&S_IRGRP)?"r":"-");
+                    FILES[counter].permissions+=((statbuff.st_mode&S_IWGRP)?"w":"-");
+                    FILES[counter].permissions+=((statbuff.st_mode&S_IXGRP)?"x":"-");
+                    FILES[counter].permissions+=((statbuff.st_mode&S_IROTH)?"r":"-");
+                    FILES[counter].permissions+=((statbuff.st_mode&S_IWOTH)?"w":"-");
+                    FILES[counter].permissions+=((statbuff.st_mode&S_IXOTH)?"x":"-");
+                    FILES[counter].numLinks=statbuff.st_nlink;
+                    FILES[counter].ownerID=pw->pw_name;
+                    FILES[counter].groupID=gr->gr_name;
+                    FILES[counter].fileSize=statbuff.st_size;
+                    FILES[counter].lastMod=statbuff.st_mtime;
+                    if(dname->d_name[0]=='.'){
+                         FILES[counter].isHidden=true;
+                    }
+                    else{
+                         FILES[counter].isHidden=false;
+                    }
+                    counter++;
+               }//while loop
+               //closedir(dir);
 
 
-     //my implementation of the sorting method
-     //searches through the sorted string array for file names and matches the order with the myFiles array
-          string fileName;
-          for(int i=0;i<totalEntries;i++){
-               fileName=FILES[i].name;
-               for(int j=0;j<totalEntries;j++){
-                    if(fileName.compare(sortedd[j])==0){
-                         FILES[i].position=j;
-                         //cout<<FILES[i].name<<"\t"<<j<<endl;
-                         break;
+          //my implementation of the sorting method
+          //searches through the sorted string array for file names and matches the order with the myFiles array
+               string fileName;
+               for(int i=0;i<totalEntries;i++){
+                    fileName=FILES[i].name;
+                    for(int j=0;j<totalEntries;j++){
+                         if(fileName.compare(sortedd[j])==0){
+                              FILES[i].position=j;
+                              //cout<<FILES[i].name<<"\t"<<j<<endl;
+                              break;
+                         }
                     }
                }
-          }
-     //I am the King Midas of spaghetti-code
+          //I am the King Midas of spaghetti-code
 
-     //prints out given the option of having no additional arguments
-          if (option.compare("none")==0){//No additional arguments
-               for(int i=0;i<totalEntries;i++){
-                    for(int j=0;j<totalEntries;j++){
-                         if(FILES[j].position==i){
-                              if(!FILES[j].isHidden){
+          //prints out given the option of having no additional arguments
+               if (option.compare("none")==0){//No additional arguments
+                    for(int i=0;i<totalEntries;i++){
+                         for(int j=0;j<totalEntries;j++){
+                              if(FILES[j].position==i){
+                                   if(!FILES[j].isHidden){
+                                        cout<<FILES[j].name<<endl;
+                                   }
+                                   break;
+                              }
+                         }
+                    }
+
+               }//No additional arguments
+               //prints out given the argument -a for all
+               else if(option.compare("-a")==0){//List all
+                    for(int i=0;i<totalEntries;i++){
+                         for(int j=0;j<totalEntries;j++){
+                              if(FILES[j].position==i){
                                    cout<<FILES[j].name<<endl;
+                                   break;
                               }
-                              break;
+                         }
+                    }
+               }
+               //prints out given the argument -l for long list
+               else if(option.compare("-l")==0){//List all
+                    for(int i=0;i<totalEntries;i++){
+                         for(int j=0;j<totalEntries;j++){
+                              if(FILES[j].position==i){
+                                   if(FILES[j].isHidden){
+                                        continue;
+                                   }
+                                   char buff[32];
+                                   struct tm *timeinfo;
+                                   timeinfo=localtime(&FILES[j].lastMod);
+                                   strftime(buff, 32, "%b %d %H:%M", timeinfo);
+                                   cout<<FILES[j].permissions<<".\t"<<FILES[j].numLinks<<"\t"<<FILES[j].ownerID<<"\t"<<FILES[j].groupID<<"\t"<<FILES[j].fileSize<<"\t";
+                                   printf("%s",buff);
+                                   cout<<"\t"<<FILES[j].name<<endl;
+                                   break;
+                              }
                          }
                     }
                }
 
-          }//No additional arguments
-          //prints out given the argument -a for all
-          else if(option.compare("-a")==0){//List all
-               for(int i=0;i<totalEntries;i++){
-                    for(int j=0;j<totalEntries;j++){
-                         if(FILES[j].position==i){
-                              cout<<FILES[j].name<<endl;
-                              break;
-                         }
-                    }
-               }
-          }
-          //prints out given the argument -l for long list
-          else if(option.compare("-l")==0){//List all
-               for(int i=0;i<totalEntries;i++){
-                    for(int j=0;j<totalEntries;j++){
-                         if(FILES[j].position==i){
-                              if(FILES[j].isHidden){
-                                   continue;
+               //prints out given the option for both long list and all
+               else if(option.compare("-la")==0||option.compare("-al")==0){
+                    for(int i=0;i<totalEntries;i++){
+                         for(int j=0;j<totalEntries;j++){
+                              if(FILES[j].position==i){
+                                   char buff[32];
+                                   struct tm *timeinfo;
+                                   timeinfo=localtime(&FILES[j].lastMod);
+                                   strftime(buff, 32, "%b %d %H:%M", timeinfo);
+                                   cout<<FILES[j].permissions<<".\t"<<FILES[j].numLinks<<"\t"<<FILES[j].ownerID<<"\t"<<FILES[j].groupID<<"\t"<<FILES[j].fileSize<<"\t";
+                                   printf("%s",buff);
+                                   cout<<"\t"<<FILES[j].name<<endl;
+                                   break;
                               }
-                              char buff[32];
-                              struct tm *timeinfo;
-                              timeinfo=localtime(&FILES[j].lastMod);
-                              strftime(buff, 32, "%b %d %H:%M", timeinfo);
-                              cout<<FILES[j].permissions<<".\t"<<FILES[j].numLinks<<"\t"<<FILES[j].ownerID<<"\t"<<FILES[j].groupID<<"\t"<<FILES[j].fileSize<<"\t";
-                              printf("%s",buff);
-                              cout<<"\t"<<FILES[j].name<<endl;
-                              break;
                          }
                     }
                }
+               closedir(dir);
+               //tries to clean up
+               delete[] sortedd;
+               delete[] FILES;
           }
-
-          //prints out given the option for both long list and all
-          else if(option.compare("-la")==0||option.compare("-al")==0){
-               for(int i=0;i<totalEntries;i++){
-                    for(int j=0;j<totalEntries;j++){
-                         if(FILES[j].position==i){
-                              char buff[32];
-                              struct tm *timeinfo;
-                              timeinfo=localtime(&FILES[j].lastMod);
-                              strftime(buff, 32, "%b %d %H:%M", timeinfo);
-                              cout<<FILES[j].permissions<<".\t"<<FILES[j].numLinks<<"\t"<<FILES[j].ownerID<<"\t"<<FILES[j].groupID<<"\t"<<FILES[j].fileSize<<"\t";
-                              printf("%s",buff);
-                              cout<<"\t"<<FILES[j].name<<endl;
-                              break;
-                         }
-                    }
-               }
-          }
-          closedir(dir);
-          //tries to clean up
-          delete[] sortedd;
-          delete[] FILES;
 }
 
 /**
